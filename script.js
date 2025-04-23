4ca4bfe409423b9cbad1839b223cf431e530e2dd
Julius <188955756+4483-s@users.noreply.github.com>
2025-04-23 11:06:12 +0800

    Improve UI and add some JS

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-const checks = document.querySelectorAll(".check");
-checks.forEach((x) => console.log(x.id));
+const btn = document.querySelector(".btn");
+const lowercaseCh = document.querySelector("#lowercase");
+const uppercaseCh = document.querySelector("#uppercase");
+const numberCh = document.querySelector("#number");
+const specialCh = document.querySelector("#special");
+const result = document.querySelector(".result");
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = lowercase.toUpperCase();
+const number = "1234567890";
+const specialChar = `!@#$%^&*()_+=-"';:/.,<>?\\|~`;
+btn.addEventListener("click", function () {
+  generate();
+});
+
+function generate() {
+  const list = "";
+  if (lowercaseCh.checked) {
+    list += lowercase;
+  }
+  if (uppercaseCh.checked) {
+    list += uppercase;
+  }
+  if (numberCh.checked) {
+    list += number;
+  }
+  if (specialCh.checked) {
+    list += specialChar;
+  }
+  result.textContent = 8;
+}
